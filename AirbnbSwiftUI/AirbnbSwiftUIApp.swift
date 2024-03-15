b1cb7c260a97bf798508ac151d68baf4334aed2b
renesalomone <108975981+renesalomone@users.noreply.github.com>
2024-03-15 07:23:55 +0700

    Initial Commit

---
+++ AirbnbSwiftUI/AirbnbSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbSwiftUIApp.swift
+//  AirbnbSwiftUI
+//
+//  Created by Rene Salomone on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
